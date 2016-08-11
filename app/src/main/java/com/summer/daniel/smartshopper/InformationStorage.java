60a667411eb6ad15ff28012612dc1bd72983ca51
= <d.hellstrom.94@gmail.com>
2016-08-11 10:57:54 +0200

    begin implementing methods for content values

--- app/src/main/java/com/summer/daniel/smartshopper/InformationStorage.java
+++ app/src/main/java/com/summer/daniel/smartshopper/InformationStorage.java
@@ -1,7 +1,10 @@
 package com.summer.daniel.smartshopper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.summer.daniel.smartshopper.database.DbSchema.StoreTable;
 
 /**
  * Created by Daniel on 2016-08-10.
@@ -21,7 +24,25 @@ public class InformationStorage {
 
     private InformationStorage(Context context){
         mContext = context.getApplicationContext();
+    }
 
+    private static ContentValues getContentValues(Store store){
+        ContentValues values = new ContentValues();
+        values.put(StoreTable.Cols.STORE_NAME, store.getName());
+        values.put(StoreTable.Cols.LATITUDE, store.getLocation().latitude);
+        values.put(StoreTable.Cols.LONGITUDE, store.getLocation().longitude);
+        values.put(StoreTable.Cols.CATEGORIES, transformArrayToString(store.getCategories()));
+        return values;
+    }
 
+    private static String transformArrayToString(String[] categories){
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < categories.length; i++){
+            builder.append(categories[i]);
+            if(i < categories.length - 1){
+                builder.append(";");
+            }
+        }
+        return builder.toString();
     }
 }
